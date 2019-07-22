4427a55a7d1317c4584f7a63d37b215d361dce01
tomohiro.togo <toto.bz1783@gmail.com>
2019-07-22 05:55:20 +0000

    add column

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,11 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  
   def index
     @tasks = Task.all
   end
   
   def show
-    @task = Task.find(params[:id])
   end
   
   def new
@@ -13,22 +14,20 @@ class TasksController < ApplicationController
   
   def create
     @task = Task.new(task_params)
-  
+    
     if @task.save
       flash[:success] = 'タスクが正常に投稿されました'
       redirect_to @task
     else
-      flash.new[:danger] = 'タスクが投稿されませんでした'
+      flash.now[:danger] = 'タスクが投稿されませんでした'
       render :new
     end
   end
   
   def edit
-    @task = Task.find(params[:id])
   end
   
   def update
-    @task = Task.find(params[:id])
   
     if @task.update(task_params)
       flash[:success] = 'task は正常に更新されました'
@@ -40,7 +39,6 @@ class TasksController < ApplicationController
   end
   
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
     
     flash[:success] = 'task は正常に削除されました'
@@ -49,7 +47,10 @@ class TasksController < ApplicationController
   
   private
   
+  def set_task
+    @task = Task.find(params[:id])
+  end
   def task_params
-    params.require(:task).permit(:content)
+    params.require(:task).permit(:content, :status)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_19_055505) do
+ActiveRecord::Schema.define(version: 2019_07_22_044700) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
   end
 
 end
